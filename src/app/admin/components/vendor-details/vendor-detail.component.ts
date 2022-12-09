9868c2f70d4395ee23fe13cc64418fe08bf2a5f0
Shreyas Udupa <shreyas.udupa@siemens.com>
2022-12-09 12:39:46 +0530

    vendor-detail sample change

--- src/app/admin/components/vendor-details/vendor-detail.component.ts
+++ src/app/admin/components/vendor-details/vendor-detail.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { BaseComponent } from "src/app/common/components/base/base.component";
 import { CommonDataSharingService } from "src/app/common/services/common-datasharing.service";
 import { MenuService } from "src/app/common/services/menu.service";
@@ -16,21 +16,26 @@ vendorId!:string;
         public menuService:MenuService,
         public override httpclient:HttpClient,
         public commonBroadcastService:CommonDataSharingService,
-        private activatedRoute:ActivatedRoute){
+        private activatedRoute:ActivatedRoute,
+        private router:Router){
             super(menuService,httpclient,commonBroadcastService)
     }
 
     ngOnInit(): void {
-        debugger
+        //debugger
         this.componentName = this.activatedRoute.snapshot.routeConfig?.component?.name;
 
         this.InitilizeMenu();
 
-        let vendoreIdState = history.state.navigationId;
+        let vendoreIdState = history.state.vendorId;
         if(vendoreIdState !== undefined){
             this.vendorId = vendoreIdState;
         }else{
             alert('undefined')
         }
+    }
+
+    goBackToVendor(){
+        this.router.navigateByUrl('/admin/vendor');
     }
 }
